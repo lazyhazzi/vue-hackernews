b592f2e8c0dcbbd5c7a013ec095c31d3a4dd1f62
hazzi <lazyhazzi@gmail.com>
2021-01-03 11:44:12 +0900

    오류 수정
    promise return  처리

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,25 +1,25 @@
 import {
   fetchList,
-  fetchNewsList,
-  fetchAskList,
-  fetchJobsList,
   fetchUserInfo,
   fetchItem
 } from "../api/index.js";
 
 export default {
   FETCH_LIST({ commit }, pageName) {
-    fetchList(pageName)
-      .then(({ data }) => commit('SET_LIST', data))
+    return fetchList(pageName)
+      .then((response) => {
+        commit('SET_LIST', response.data)
+        return response;
+      })
       .catch(error => console.log(error));
   },
   FETCH_USER({ commit }, username) {
-    fetchUserInfo(username)
+    return fetchUserInfo(username)
       .then(({ data }) => commit('SET_USER', data))
       .catch(error => console.log(error))
   },
   FETCH_ITEM({ commit }, id) {
-    fetchItem(id)
+    return fetchItem(id)
       .then(({ data }) => commit('SET_ITEM', data))
       .catch(error => console.log(error))
   }
