470408a28fb1e860297ff01ffa31f2e38779eadf
hazzi <lazyhazzi@gmail.com>
2021-01-03 13:00:50 +0900

    리팩토링-async,await
    오류나서 주석처리

--- src/api/index.js
+++ src/api/index.js
@@ -9,6 +9,13 @@ const config = {
 function fetchList(pageName) {
     return axios.get(`${config.baseUrl}/${pageName}/1.json`);
 }
+// async function fetchList(pageName) {
+//     try {
+//         return await axios.get(`${config.baseUrl}/${pageName}/1.json`);
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
 function fetchUserInfo(username) {
     return axios.get(`${config.baseUrl}/user/${username}.json`)
 }
--- src/store/actions.js
+++ src/store/actions.js
@@ -5,6 +5,7 @@ import {
 } from "../api/index.js";
 
 export default {
+  // promise
   FETCH_LIST({ commit }, pageName) {
     return fetchList(pageName)
       .then((response) => {
@@ -13,6 +14,13 @@ export default {
       })
       .catch(error => console.log(error));
   },
+
+  // async
+  // async FETCH_LIST({ commit }, pageName) {
+  //   const response = await fetchList(pageName);
+  //   commit('SET_LIST', response.data);
+  //   return response;
+  // },
   FETCH_USER({ commit }, username) {
     return fetchUserInfo(username)
       .then(({ data }) => commit('SET_USER', data))
